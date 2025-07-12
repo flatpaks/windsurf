4d739d8f2f604fa6a2ca2db59758129f3a1b3dce
user <user@server>
2025-07-12 07:19:25 -0500

    fix launch script

--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-export ELECTRON_RUN_AS_NODE=1
-exec /app/share/windsurf/windsurf /app/share/windsurf/resources/app/out/cli.js "$@"
+exec zypak-wrapper /app/share/windsurf/windsurf /app/share/windsurf/resources/app/out/cli.js "$@"
